60c29ce64f50cc0515b284b6c9850f68f43ef5c4
Farhan160200 <fadhlurrahmanfarhan16@gmail.com>
2022-12-14 13:37:50 +0700

    add note fragment

---
+++ app/src/main/java/com/farhanfarkaann/finalprojectskripsi/note/NoteDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.farhanfarkaann.finalprojectskripsi.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.farhanfarkaann.finalprojectskripsi.R
+import com.farhanfarkaann.finalprojectskripsi.databinding.FragmentNoteListingBinding
+
+
+class NoteDetailFragment : Fragment() {
+
+    lateinit var binding : FragmentNoteListingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNoteListingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
